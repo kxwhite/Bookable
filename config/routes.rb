89510653a57ecbe6aefe17c25903713d4001e7c4
Chris Webster <chriswebster1985@googlemail.com>
2022-02-01 19:18:31 +0000

    authentication setup done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lessons, only: [:index]
+
+  # just putting this here so we know the code for making the lessons page only visible after login
+  get 'profile', to: 'pages#profile'
 end
