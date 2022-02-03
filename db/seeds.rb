08f747efeb8b1d109ba918d711e5fbaabe96bdc2
Claire Lo <caudelan@gmail.com>
2022-02-03 16:05:29 +0000

    added faker to gemfile, created seeds, added first_name, last_name and username to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up database"
+User.destroy_all
+Lesson.destroy_all
+Booking.destroy_all
+puts "Database is clean"
+
+puts "Creating 10 users"
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Lorem.unique.word,
+    email: Faker::Internet.email,
+    password: Faker::Internet.unique.password
+  )
+  puts "User #{user.id} is created"
+end
+
+puts "Creating 20 lessons"
+20.times do
+  lesson = Lesson.create(
+    title: Faker::Lorem.words(number: 4),
+    description: Faker::Lorem.paragraph,
+    location: Faker::Nation.capital_city,
+    date: Faker::Date.forward(days: 100),
+    time: "#{rand(7..21)}h",
+    user_id: rand(1..10)
+  )
+  puts "Lesson #{lesson.id} is created"
+end
+
+puts "Done!"
+
+puts "Creating 5 bookingss"
+5.times do
+  booking = Booking.create(
+    user_id: rand(1..10),
+    lesson_id: rand(1..20)
+  )
+  puts "Booking #{booking.id} is created"
+end
+
+puts "All done!"
