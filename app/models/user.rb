ccebc2ea73dfb6cca238a889b947b1ba4fc748f1
Keon White <keon-1@hotmail.co.uk>
2022-02-05 13:07:16 +0000

    Fixed the seed error. Profile index page links fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :username, presence: true
-  validates :username, :password, uniqueness: true
+  validates :username, uniqueness: true
 end
