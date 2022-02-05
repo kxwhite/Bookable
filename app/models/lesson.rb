a41af0f9b82653e62de8d44a32502cd8c3b3e9dd
Keon White <keon-1@hotmail.co.uk>
2022-02-05 10:52:58 +0000

    Booking time and date seed change from lesson. Show page booking form. New Alert

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,5 @@ class Lesson < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  validates :title, :description, :location, :date, :time, presence: true
+  validates :title, :description, :location, presence: true
 end
