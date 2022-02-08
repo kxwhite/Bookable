f75234789cdafb19501cef1319a8a3595639bbbd
Keon White <keon-1@hotmail.co.uk>
2022-02-08 21:33:18 +0000

    Map creation

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,13 @@ class LessonsController < ApplicationController
 
   def index
     @lessons = Lesson.all
+
+    @markers = @lessons.geocoded.map do |lesson|
+      {
+        lat: lesson.latitude,
+        lng: lesson.longitude
+      }
+    end
   end
 
   def show
