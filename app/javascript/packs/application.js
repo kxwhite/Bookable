586132a0a61a59d213bbe608ac2be6f1ec2dd680
Claire Lo <caudelan@gmail.com>
2022-02-11 21:10:12 +0000

    made current tab active

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,13 +20,15 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initNavbarActive } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  // initNavbarActive();
 });
