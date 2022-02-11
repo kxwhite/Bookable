1782949c85a2dfb206381f4fd80ee1d89f9efdbb
Claire Lo <caudelan@gmail.com>
2022-02-11 23:15:43 +0000

    refactored code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,10 +23,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-// import { initProfileTabs } from '../components/tab';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  // initProfileTabs();
 });
