06fbe096cf6b3da8f37476d34fa629696233ab94
Claire Lo <caudelan@gmail.com>
2022-02-11 21:45:38 +0000

    refactored code to solve issue of map not showing on lesson index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,6 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
-import { initNavbarActive } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
